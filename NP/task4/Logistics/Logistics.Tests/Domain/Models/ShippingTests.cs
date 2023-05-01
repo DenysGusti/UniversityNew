53171375a497d59ad9377ef39d3a3b3f6e1276f7
DenysGusti <denys-lev.gusti@lnu.edu.ua>
2023-05-01 03:44:41 +0300

    added unit tests and linq

---
+++ NP/task4/Logistics/Logistics.Tests/Domain/Models/ShippingTests.cs
@@ -0,0 +1,25 @@
+﻿using Logistics.Domain.Models;
+
+namespace Logistics.Tests.Domain.Models;
+
+[TestClass]
+public class ShippingTests
+{
+    [TestMethod]
+    public void TestTooShortRepresentationStringShipping_ReturnsException() =>
+        Assert.ThrowsException<IndexOutOfRangeException>(() => 
+            new Shipping().Convert(
+                "1 Honey 16,5 Sklad_5 Lviv 250 Depot_7 Amsterdam 2000 Oleksandr Kovalenko 34 3".Split()));
+    
+    [TestMethod]
+    public void TestWrongTransportRepresentationStringShipping_ReturnsException() =>
+        Assert.ThrowsException<ArgumentException>(() => 
+            new Shipping().Convert(
+                "1 Honey 16,5 Sklad_5 Lviv 250 Depot_7 Amsterdam 2000 Oleksandr Kovalenko 34 3 road".Split()));
+    
+    [TestMethod]
+    public void TestWrongFormatRepresentationStringShipping_ReturnsException() =>
+        Assert.ThrowsException<FormatException>(() => 
+            new Shipping().Convert(
+                "1 Honey 16.5 Sklad_5 Lviv 250 Depot_7 Amsterdam 2000 Oleksandr Kovalenko 34 3 road".Split()));
+}
--- NP/task4/Logistics/Logistics.Tests/Domain/Repositories/MemoryLogisticsRepositoryTests.cs
+++ NP/task4/Logistics/Logistics.Tests/Domain/Repositories/MemoryLogisticsRepositoryTests.cs
@@ -19,9 +19,87 @@ public class MemoryLogisticsRepositoryTests
             new Driver("Peter", "Morgan", 52), new Plane());
         
         memoryRepository.Create(shippingToAdd);
-
-        var shippingAdded = memoryRepository.GetById(4);
+        
+        var shippingAdded = memoryRepository.GetAll()[^1];
 
         Assert.AreEqual(shippingToAdd, shippingAdded);
+    }
+    
+    [TestMethod]
+    public void TestEditShipping_ReturnsAddedShipping()
+    {
+        MemoryRepository<Shipping> memoryRepository = new();
+        
+        Shipping shippingToAdd = new (4, 2, new Goods("Hoe", 2m),
+            new Warehouse("Sklad_3", "New_York", 1000m),
+            new Warehouse("Depot_3", "Warsaw", 800m),
+            new Driver("Peter", "Morgan", 52), new Plane());
+        
+        memoryRepository.Create(shippingToAdd);
+
+        Shipping shippingEdited = new (4, 222, new Goods("Hoe", 2m),
+            new Warehouse("Sklad_3", "New_York", 1000m),
+            new Warehouse("Depot_3", "Warsaw", 800m),
+            new Driver("Peter", "Morgan", 52), new Plane());
+        
+        memoryRepository.Update(shippingEdited);
+
+        Assert.AreEqual(shippingEdited, memoryRepository.GetById(4));
+    }
+    
+    [TestMethod]
+    public void TestDeleteShipping_ReturnsLength()
+    {
+        MemoryRepository<Shipping> memoryRepository = new();
+        
+        Shipping shipping = new (4, 2, new Goods("Hoe", 2m),
+            new Warehouse("Sklad_3", "New_York", 1000m),
+            new Warehouse("Depot_3", "Warsaw", 800m),
+            new Driver("Peter", "Morgan", 52), new Plane());
+
+        memoryRepository.Create(shipping);
+
+        Assert.AreEqual(1, memoryRepository.GetAll().Length);
+
+        memoryRepository.Delete(shipping);
+
+        Assert.AreEqual(0, memoryRepository.GetAll().Length);
+    }
+    
+    [TestMethod]
+    public void TestGetById_ReturnsShipping()
+    {
+        MemoryRepository<Shipping> memoryRepository = new();
+        
+        Shipping shipping = new (4, 2, new Goods("Hoe", 2m),
+            new Warehouse("Sklad_3", "New_York", 1000m),
+            new Warehouse("Depot_3", "Warsaw", 800m),
+            new Driver("Peter", "Morgan", 52), new Plane());
+
+        memoryRepository.Create(shipping);
+
+        Assert.AreEqual(shipping, memoryRepository.GetById(shipping.Id));
+    }
+    
+    [TestMethod]
+    public void TestGetAll_ReturnsShippingArray()
+    {
+        MemoryRepository<Shipping> memoryRepository = new();
+        
+        Shipping shipping1 = new (1, 2, new Goods("Hoe", 2m),
+            new Warehouse("Sklad_3", "New_York", 1000m),
+            new Warehouse("Depot_3", "Warsaw", 800m),
+            new Driver("Peter", "Morgan", 52), new Plane());
+
+        Shipping shipping2 = new(4, 2, new Goods("Hoe", 2m),
+            new Warehouse("Sklad_3", "New_York", 1000m),
+            new Warehouse("Depot_3", "Warsaw", 800m),
+            new Driver("Peter", "Morgan", 52), new Plane());
+
+        memoryRepository.Create(shipping1);
+        memoryRepository.Create(shipping2);
+
+        Assert.AreEqual(memoryRepository.GetAll()[^1], memoryRepository.GetById(shipping2.Id));
+        Assert.AreEqual(2, memoryRepository.GetAll().Length);
     }
 }

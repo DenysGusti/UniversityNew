35cb459ceff74299146f86847167432b84039588
Denys Gusti <90290236+DenysGusti@users.noreply.github.com>
2023-05-07 05:10:12 +0300

    added task 6 pz

---
+++ PZ/task_06/task_06/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace task_06;
+
+/*
+6.28 Прочитати текстову стрічку довжиною не більше ніж 253 символи.
+Перевірити правильність входження у текст квадратних та фігурних дужок (ситуації } { або [{]} - є помилковими).
+*/
+
+internal static class Program {
+    private static bool CheckBracketsCorrectness(string s)
+    {
+        Stack<char> brackets = new();
+        foreach (var c in s)
+        {
+            switch (c)
+            {
+                case '[' or '{':
+                    brackets.Push(c);
+                    break;
+                case ']' or '}':
+                    if (brackets.Count == 0 || brackets.Pop() != (c == ']' ? '[' : '{'))
+                        return false;
+                    break;
+            }
+        }
+        return brackets.Count == 0;
+    }
+    
+    public static void Main()
+    {
+        var s = Console.ReadLine()!;
+        Console.WriteLine($"Brackets are correct: {CheckBracketsCorrectness(s.Length > 253 ? s[..253] : s)}");
+    }
+}

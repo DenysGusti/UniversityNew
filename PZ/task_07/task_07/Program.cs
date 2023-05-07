c614bfc57a38016c84949ad1ee64c81e5e243576
denys <denys.gusti65@gmai.com>
2023-05-07 18:27:05 +0300

    added task 7 pz

---
+++ PZ/task_07/task_07/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+
+namespace task_07;
+
+internal static class Program {
+
+    public static void Main(string[] args)
+    {
+        using var process = new Process {StartInfo = new ProcessStartInfo
+        {
+            FileName = "chmod",
+            Arguments = string.Join(' ', args),
+            UseShellExecute = false,
+            RedirectStandardOutput = true
+        }};
+        process.Start();
+
+        // Synchronously read the standard output of the spawned process.
+        var reader = process.StandardOutput;
+        var output = reader.ReadToEnd();
+
+        // Write the redirected output to this application's window.
+        Console.WriteLine(output);
+
+        process.WaitForExit();
+
+        // Console.WriteLine("\n\nPress any key to exit.");
+        // Console.ReadLine();
+    }
+}
